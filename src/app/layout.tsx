713ed7f616b43816e0f67fd5ccb98cacdf9ce4b7
Abdul Mannan <abdul-m107@proton.me>
2025-10-09 00:33:30 +0530

    feat: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,34 @@
 import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
-import { Montserrat } from "next/font/google"; // Montserrat import kiya
+import { Montserrat } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
-// import TopBanner from "@/components/TopBanner";
-// import Navbar from "@/components/Navbar";
 
-// Montserrat font ko setup kiya
 const montserrat = Montserrat({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"], // Aap zaroorat ke hisaab se weights add kar sakte hain
-  variable: "--font-montserrat", // CSS variable ka naam
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-montserrat",
 });
 
 export const metadata: Metadata = {
-  title: "Indifly",
-  description: "Building Ventures, Building Nation.",
+  title:
+    "Indifly Ventures | Empowering Entrepreneurship with Innovative Technology",
+  description:
+    "Indifly Ventures: Pioneering entrepreneurship with cutting-edge technology and strategic partnerships across emerging sectors in Bharat.",
+  keywords: [
+    "entrepreneurship",
+    "innovative technology",
+    "strategic partnerships",
+    "emerging sectors",
+    "business empowerment",
+    "technology solutions",
+    "startup support",
+    "digital transformation",
+    "venture capital",
+  ],
+  alternates: {
+    canonical: "https://www.indiflyventures.com",
+  },
 };
 
 export default function RootLayout({
