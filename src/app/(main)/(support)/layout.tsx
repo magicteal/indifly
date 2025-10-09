1f097ac60af9615cf0766507df4a8098cc6d0ce6
Abdul Mannan <abdul-m107@proton.me>
2025-10-09 16:12:34 +0530

    Add privacy policy, capitalize terms condition headings, update support layout

--- src/app/(main)/(support)/layout.tsx
+++ src/app/(main)/(support)/layout.tsx
@@ -1,6 +1,13 @@
 import Container from "@/components/container";
+import { Footer } from "@/components/layout/Footer";
+import { lightTheme } from "@/lib/serviceContext";
 import React from "react";
 
 export default function layout({ children }: { children: React.ReactNode }) {
-  return <Container className="prose">{children}</Container>;
+  return (
+    <>
+      <Container className="prose mt-36 mb-24">{children}</Container>
+      <Footer theme={lightTheme} />
+    </>
+  );
 }
