697bdab2ff60a0ad9451929cd4e53a92cbb47e5b
Abdul Mannan <abdul-m107@proton.me>
2025-10-09 16:25:04 +0530

    style: enhance layout component with additional prose styling

--- src/app/(main)/(support)/layout.tsx
+++ src/app/(main)/(support)/layout.tsx
@@ -6,7 +6,9 @@ import React from "react";
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Container className="prose mt-36 mb-24">{children}</Container>
+      <Container className="prose mt-36 mb-24 prose-h2:text-primary prose-p:text-justify prose-ol:text-justify prose-ul:text-justify">
+        {children}
+      </Container>
       <Footer theme={lightTheme} />
     </>
   );
